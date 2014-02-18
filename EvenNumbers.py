466f199437c898463a058f79036bb1a35670347d
Eden <ghirme@uw.edu>
2014-02-18 15:51:04 -0800

    python commit

---
+++ EvenNumbers.py
@@ -0,0 +1,16 @@
+# Eden Ghirmai, 2/18/20
+# Checks input numbers and 
+# determines whether a number is even or not.
+# Prints 1 if the number is even or 0 if the number is odd.
+
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    if test: 
+    	num = int(test); 
+    	if num % 2 == 0:
+    		print "1"; 
+    	else: 
+    		print "0"; 	
+
+test_cases.close()
