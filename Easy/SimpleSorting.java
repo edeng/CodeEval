b0aa5cdd7e4d6b3051ae5ff7f331436e0a486a4d
Eden Ghirmai <ghirme@uw.edu>
2014-03-04 11:49:44 -0800

    Update SimpleSorting.java

--- Easy/SimpleSorting.java
+++ Easy/SimpleSorting.java
@@ -6,23 +6,23 @@ import java.util.*;
 
 public class SimpleSorting {
     public static void main(String[] args) throws FileNotFoundException {
-		File file = new File(args[0]);
-		Scanner in = new Scanner(file);
+	File file = new File(args[0]);
+	Scanner in = new Scanner(file);
 		
-		while(in.hasNextLine()) {
-			String[] all = in.nextLine().trim().split("[ \t]+");
-			Map<Double, String> map = new TreeMap<Double, String>(); 
-			for (int i = 0; i < all.length; i++) {
-				double num = Double.parseDouble(all[i]);
-				String numString = all[i]; 
-				map.put(num,  numString); 
-			}
-						
-			for (double num : map.keySet()) {
-				System.out.print(map.get(num) + " " );
-			}
-			
-			System.out.println(); 
+	while(in.hasNextLine()) {
+		String[] all = in.nextLine().trim().split("[ \t]+");
+		Map<Double, String> map = new TreeMap<Double, String>(); 
+		for (int i = 0; i < all.length; i++) {
+			double num = Double.parseDouble(all[i]);
+			String numString = all[i]; 
+			map.put(num,  numString); 
 		}
+						
+		for (double num : map.keySet()) {
+			System.out.print(map.get(num) + " " );
+		}
+			
+		System.out.println(); 
+	}
     }
 }
